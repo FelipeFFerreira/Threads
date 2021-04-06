bd1ada1e29d35519ad3ed02e17d60fb165b9197e
Felipe Ferreira <eng.ffn@gmail.com>
2021-04-05 23:21:19 -0300

    Add OpemMP

--- main.c
+++ main.c
@@ -4,12 +4,19 @@
 #include <unistd.h>
 #include <time.h>
 #include "lista.h"
-#include <omp.h>
+
+//#define INSTALL_OMP //Se for no linux INSTALL
+
+#ifdef INSTALL_OMP
+	#include <omp.h>
+#endif
 
 #define rand1() (rand() / (tipoDado)RAND_MAX)
 
-#define N 500000000 //DEFINA O TAMANO DA MATRIZ AQUI
-#define threads 6 //DEFINA A QUANTIDADE DE THREADS AQUI
+#define N 1000 //DEFINA O TAMANO DA MATRIZ AQUI
+#define threads 8 //DEFINA A QUANTIDADE DE THREADS AQUI
+
+/*semaphoro controle de IO*/
 #define WAIT 0
 #define READY 1
 #define EXECUTED -1
@@ -20,9 +27,9 @@ typedef struct {
 }semaphore;
 semaphore sem_control;
 
-typedef int tipoDado;
+typedef unsigned long long int tipoDado;
 tipoDado matrizResultante[N][N], matriz_1[N][N];
-int matrizIndentidade[N][N];
+tipoDado matrizIndentidade[N][N];
 
 typedef struct argsArq * ptrArgsArq;
 struct argsArq {
@@ -43,17 +50,17 @@ typedef struct {
 
 void * solicitacaoArquivo(void * argsArq)
 {
-	clock_t tempo;
-	tempo = clock();
-	int i = 0; 
-	int j, pronto = N;
+	//clock_t tempo;
+	//tempo = clock();
+	tipoDado i = 0; 
+	tipoDado j, pronto = N;
 	ptrArgsArq _argssArq = (ptrArgsArq) argsArq;
 
 	while (i < N) {
 		if (_argssArq->statusArq[i] == READY) {
 			for (j = 0; j < N; j++) {
 				while(sem_control.state);
-				fprintf(_argssArq->arq, "%d  ", matrizResultante[i][j]);
+				fprintf(_argssArq->arq, "%lu  ", matrizResultante[i][j]);
 			}
 			fprintf(_argssArq->arq, "%c", '\n'); 
 			_argssArq->statusArq[i] = EXECUTED;
@@ -62,7 +69,7 @@ void * solicitacaoArquivo(void * argsArq)
 	}
 	
 	fclose(_argssArq->arq);
-	printf("\n>>>>Tempo Total Do Processo De Escrita no Arquivo: %gs\n",(float) (clock() - tempo)  / CLOCKS_PER_SEC);
+	//printf("\n>>>>Tempo Total Do Processo De Escrita no Arquivo: %gs\n",(float) (clock() - tempo)  / CLOCKS_PER_SEC);
 
 	//printf("\nAcabei de escrever\n");
 	pthread_exit( (void*) 0 );		//Legado do retorno
@@ -74,13 +81,21 @@ void * multiplicacao(void * argss)
 	
     args * _argss = (args *)argss;
     lst_ptr p;
-	int j;
+	tipoDado j;
 	p = _argss->lista;
+
+	int k = 0;
 	
 	while (p != NULL) {
+		#ifdef INSTALL_OMP
+			#pragma omp parallel for
+		#endif
 		for (j = 0;  j < N; j++) {
-			matrizResultante[p->dado][j] += matriz_1[p->dado][j] * matrizIndentidade[p->dado][j];
+			for (k = 0; k < N; k++) {
+					matrizResultante[p->dado][j] = matrizResultante[p->dado][j] + matriz_1[p->dado][k] * matrizIndentidade[k][j];										
+			}
 		}
+	
 		_argss->ptrArq->statusArq[p->dado] = READY; //libera para escrita no arquivo
 		p = p->prox;
 	}
@@ -90,50 +105,41 @@ void * multiplicacao(void * argss)
 }
 
 
-void criarMatrizIdentidade()
+void print_matriz(tipoDado matriz[][N])
 {
-	int i, j;
+	tipoDado i, j;
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++)
-			matrizIndentidade[i][j] = j + 1;
+			printf("%lu", matriz[i][j]);
 	}
+	printf("\n");
 }
 
-void criarMatrizRandomica()
+void * lerMatrizEntrada_1(void * fptr)
 {
-	
-	int i, j;
-	for(i = 0; i < N; i++) {
-		for(j = 0 ; j < N; j++) {
-			matriz_1[i][j] = j + 1; //rand1();
+	FILE * fptr_ = (FILE *)fptr;
+	tipoDado i, j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			fscanf(fptr_, "%d ", &matrizIndentidade[i][j]);
 		}
 	}
 }
 
-void imprimirMatriz(tipoDado matriz[][N])
-{	
-	FILE * arq; // Arquivo lógico     
-    if ((arq = fopen("result_mult_matriz.txt", "w")) == NULL) {
-        fprintf(stderr, "Erro na abertura do arquivo %s\n", "filename");
-     }
-
-	int i, j;
-	printf("\n");
-	for(i = 0; i < N; i++) {
-		for(j = 0 ; j < N; j++) {
-			printf("%d  ", matriz[i][j]);
-			fprintf(arq, "%d", matriz[i][j]);
-		}
-		printf("\n");
-		fprintf(arq, "\n");
+void * lerMatrizEntrada_2(void * fptr)
+{
+	FILE * fptr_ = (FILE *)fptr;
+	tipoDado i, j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++)
+			fscanf(fptr_, "%lu", &matriz_1[i][j]);
 	}
-	printf("\n");
 }
 
-FILE *open_arquivo(char * c) {
+FILE *open_arquivo(char * str, char * modo) {
 
     FILE * arq; // Arquivo lógico     
-    if ((arq = fopen("result_mult_matriz_th.csv", c)) == NULL) {
+    if ((arq = fopen(str, modo)) == NULL) {
         fprintf(stderr, "Erro na abertura do arquivo %s\n", "filename");
      }
 
@@ -155,15 +161,38 @@ void print_responsabilidade_thread(args * _args)
 
 int main ()
 {
-	int i, j, status;
+	tipoDado i, j, status;
 	clock_t tempo;
 	args _args[threads - 1]; //numero de args por threads de CPU
-	struct argsArq _argsArq; //thread responsavel pelo arquivo
+	struct argsArq _argsArq; //thread responsavel pelo arquivo de saida
 
-	criarMatrizIdentidade();
-	criarMatrizRandomica();
+	pthread_t thread_1; //thread responsavel pelo arquivo de entrada
+	pthread_t thread_2; //thread responsavel pelo arquivo de entrada
 
-	_argsArq.arq = open_arquivo("w");
+	FILE * fptr_1 = open_arquivo("/home/felipe/Faculdade/Programa Paralela/Threads/Matrizes/matriz_1.txt", "r");
+	
+	FILE * fptr_2 = open_arquivo("/home/felipe/Faculdade/Programa Paralela/Threads/Matrizes/matriz_2.txt", "r");
+	
+
+	status = pthread_create((&thread_1), NULL, lerMatrizEntrada_1, (void *)fptr_1);
+	if ( status ) {
+		printf("Erro criando thread %d, retornou codigo %d\n", i, status );
+		return EXIT_FAILURE;
+	}
+
+	status = pthread_create((&thread_2), NULL, lerMatrizEntrada_2, (void *)fptr_2);
+	if ( status ) {
+		printf("Erro criando thread %d, retornou codigo %d\n", i, status );
+		return EXIT_FAILURE;
+	}
+
+	pthread_join(thread_1, NULL);
+	pthread_join(thread_2, NULL);
+
+	fclose(fptr_1);
+	fclose(fptr_2);
+
+	_argsArq.arq = open_arquivo("matriz_resultante.csv", "w");
 
 	//#pragma omp for
 	for(i = 0; i < N; i++) _argsArq.statusArq[i] = WAIT;
@@ -181,7 +210,8 @@ int main ()
 	for(i = 0; i < N; i++) lst_ins(&_args[i % (threads - 1)].lista, i);
 	
 	/*Repassa função de trabalho*/
-	tempo = clock();	
+	tempo = clock();
+
 	for(i = 0; i < threads - 1; i++) {
 		_args[i].ptrArq = &_argsArq;
 		status = pthread_create((&_args[i].thread), NULL, multiplicacao, (void *)&_args[i]);
@@ -206,17 +236,18 @@ int main ()
 	}
 	pthread_join(_argsArq.thread, NULL);
 
-	fprintf(_argsArq.arq, "\n\n[Tempo Total Do Processo: %gs]", (float) (clock() - tempo)  / CLOCKS_PER_SEC);
+
+	//fprintf(_argsArq.arq, "\n\n[Tempo Total Do Processo: %gs]", (float) (clock() - tempo)  / CLOCKS_PER_SEC);
+	
+	print_responsabilidade_thread(_args);
+	
 	fclose(_argsArq.arq);
-
-	
-	//print_responsabilidade_thread(_args);
-	
-
 	//imprimirMatriz(matrizResultante);
 
 	printf("\nTerminando processo ...\n");
-	printf("Tempo Total Do Processo Para Processa %s informaçoes: %gs\n\n", (N * N) >= 1000000000 ? "1 BILHÃO" : "MENOS QUE UM BILHAO", (float) (clock() - tempo)  / CLOCKS_PER_SEC);
+	printf("\n\n[Tempo Total Do Processo: %fs]\n", (float) (clock() - tempo)  / CLOCKS_PER_SEC);
+
+	
 
 	return EXIT_SUCCESS;
 }
